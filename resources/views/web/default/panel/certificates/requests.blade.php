592591bf633511ac58feb80d32467866f4e5276c
RachaZaibi <140545678+RachaZaibi@users.noreply.github.com>
2025-04-09 18:52:49 +0100

    ans scripts links

--- resources/views/web/default/panel/certificates/requests.blade.php
+++ resources/views/web/default/panel/certificates/requests.blade.php
@@ -2,7 +2,7 @@
 
 @push('styles_top')
     <link rel="stylesheet" href="/assets/vendors/fontawesome/css/all.min.css">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/css/select2.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="/assets/default/vendors/select2/select2.min.css">
 @endpush
 
 
@@ -175,9 +175,8 @@
 @endsection
 
 @push('scripts_bottom')
-    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/js/select2.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="/assets/admin/vendor/jquery/jquery-3.3.1.min.js"></script>
+    <script src="/assets/default/vendors/select2/select2.min.js"></script>
 
     <script>
         $(document).ready(function () {
